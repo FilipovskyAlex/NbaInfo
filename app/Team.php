88714a9bb5953e6dd9500d42da328d7dba8e37cf
FilipovskyAlex <filipovskiy99@gmail.com>
2019-06-24 11:09:24 +0300

    use slug helper function instead of str_replace; add phpDocs

--- app/Team.php
+++ app/Team.php
@@ -3,19 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
+/**
+ * Class Team
+ * @package App
+ */
 class Team extends Model
 {
 
 
+    /**
+     * @param string $abbr
+     * @param string $name
+     * @return string
+     */
     public static function getFullLink(string $abbr, string $name) : string
     {
+        // If team is New Orlean Pelicans
         if($abbr === 'NOP') {
             $abbr = 'NO';
 
             return config('teamsLink.TEAM_LINK').self::abbrToLower($abbr).'/'.self::getFullName($name);
         }
 
+        // If team is Utah Jazz
         if($abbr === 'UTA') {
             $abbr = 'UTAH';
 
@@ -25,22 +37,37 @@ class Team extends Model
         return config('teamsLink.TEAM_LINK').self::abbrToLower($abbr).'/'.self::getFullName($name);
     }
 
+    /**
+     * Transform abbreviation to lowercase
+     * @param string $abbr
+     * @return string
+     */
     public static function abbrToLower(string $abbr) : string
     {
         return mb_strtolower($abbr);
     }
 
+    /**
+     * @param string $full_name
+     * @return string
+     */
     public static function getFullName(string $full_name) : string
     {
-        return str_replace(' ', '-', mb_strtolower($full_name));
+        return Str::slug($full_name, '-');
     }
 
+    /**
+     * @param $abbr
+     * @return string
+     */
     public static function getAvatar($abbr)
     {
+        // If team is New Orlean Pelicans
         if($abbr === 'NOP') {
             $abbr = 'NO';
         }
 
+        // If team is Utah Jazz
         if($abbr === 'UTA') {
             $abbr = 'UTAH';
         }
